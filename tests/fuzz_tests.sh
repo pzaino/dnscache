4168d06184b6a57c15e53eff255d47346abfc6f6
Paolo Fabio Zaino <p.zaino@zfpsystems.com>
2026-02-11 22:03:15 +0000

    Improved fuzz testing

--- tests/fuzz_tests.sh
+++ tests/fuzz_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-RUSTFLAGS="-Zsanitizer=address" cargo +nightly fuzz run fuzz_target_1 -- -max_total_time=300
+RUSTFLAGS="-Zsanitizer=address -C opt-level=1 -C debuginfo=1" cargo +nightly fuzz run fuzz_target_1 -- -max_total_time=300
 
-RUSTFLAGS="-Zsanitizer=address" cargo +nightly fuzz run fuzz_handle -- -max_total_time=300
+RUSTFLAGS="-Zsanitizer=address -C opt-level=1 -C debuginfo=1" cargo +nightly fuzz run fuzz_handle -- -max_total_time=300
 
-RUSTFLAGS="-Zsanitizer=address" cargo +nightly fuzz run fuzz_combined_parsing -- -max_total_time=300
+RUSTFLAGS="-Zsanitizer=address -C opt-level=1 -C debuginfo=1" cargo +nightly fuzz run fuzz_cache_key -- -max_total_time=300
