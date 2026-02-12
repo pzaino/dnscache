ecdf5be239856ae770c0fa9314459738565cb9ac
Paolo Fabio Zaino <p.zaino@zfpsystems.com>
2026-02-12 03:50:05 +0000

    Added file headers with license

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+/*
+ * DNS Cache Server
+ *
+ * Copyright (c) 2026 Paolo Fabio Zaino
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 use dnscache::config::Config;
 use dnscache::server::DnsCacheServer;
 
