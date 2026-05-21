a3b2b85aa3abf4abbd47e1396ebfb2a680decdea
Paolo Fabio Zaino <p.zaino@zfpsystems.com>
2026-05-21 16:16:00 +0100

    Code refactoring to adapt to new rand 0.10.1 release API chnages

--- src/config.rs
+++ src/config.rs
@@ -3,8 +3,8 @@
  *
  * Copyright (c) 2026 Paolo Fabio Zaino, all rights reserved.
  *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * This Source Code is subject to the terms of the Mozilla Public
+ * License, v2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  *
  * SPDX-License-Identifier: MPL-2.0
